82f99b76d421621326497c7a7f0b01c725b8f413
zpaki5142 <zachisaac@users.noreply.github.com>
2018-10-11 22:05:38 -0700

    seventh commit

--- liri.js
+++ liri.js
@@ -6,10 +6,14 @@ var axios = require("axios");
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
+var omdb = require('omdb');
 
+//Arguments
 var action = process.argv[2];
 var title = process.argv.slice(3).join(" ");
 
+//BANDS IN TOWN
+//Get Concert Function
 function getConcerts() {
     var queryUrl = "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp";
     axios.get(queryUrl).then(
@@ -58,7 +62,64 @@ function getSongs() {
         }
     )
 }
+
 function getMovies() {
+
+    if (!title) {
+        title = "Mr.Nobody";
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(
+        function (response) {
+            if (response.data.length === 0) {
+                console.log("No movies for " + title);
+                return;
+            }
+            console.log("Movies for " + title);
+            //response.data is an object, cannot iterate as if was array
+
+            //console.log(response.data);
+            console.log(response.data.Title + ", " + response.data.Year + ", " + response.data.Plot);
+            console.log(response.data.imdbRating);
+            //console.log("-----------------------");
+
+            if (response.data.Ratings[1]) {
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("-----------------------");
+            }
+        })
+}
+
+/*function getMovies() {
+    var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+ 
+}
+ 
+omdb.search('saw', function(err, movies) {
+    if(err) {
+        return console.error(err);
+    }
+ 
+    if(movies.length < 1) {
+        return console.log('No movies were found!');
+    }
+ 
+    movies.forEach(function(movie) {
+        console.log('%s (%d)', movie.title, movie.year);
+    });
+ 
+    // Saw (2004)
+    // Saw II (2005)
+    // Saw III (2006)
+    // Saw IV (2007)
+    // ...
+});*/
+
+
+
+
+/*function getMovies() {
     var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
     axios.get(queryUrl).then(
         function (response) {
@@ -68,14 +129,14 @@ function getMovies() {
             }
             console.log("Movies for " + title);
             for (var i = 0; i < response.data.length; i++) {
-                console.log("Location: " + response.data[i].movie.year + ", " + (response.data[i].venue.region || response.data[i].venue.country));
+                console.log("Location: " + response.data[i].title.year + ", " + (response.data[i].venue.region || response.data[i].venue.country));
                 console.log("Venue: " + response.data[i].venue.name);
                 console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
                 console.log("-----------------------");
             }
         }
     )
-}
+}*/
 function getRandom() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
